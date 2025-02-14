980e302150d41f4103c8f47ce3b4b2809eee2f81
Harshavardhan YJ <harsha@nagu.co>
2025-02-14 18:30:36 +0530

    changes in add-product page

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,10 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { Product } from '../models/product.model';
+import { CommonModule } from '@angular/common';
+import { ProductFormComponent } from '../product-form/product-form.component';
+import { MatDialog } from '@angular/material/dialog'
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table'
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-add-product',
-  imports: [],
+  imports: [CommonModule, ProductFormComponent, MatButtonModule, MatCardModule, MatTableModule],
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.scss'
 })
@@ -13,8 +19,9 @@ export class AddProductComponent implements OnInit {
   isTableView: boolean = true;
   showModal: boolean = false;
   editingProduct: Product | null =null;
+  displayedColumns: string[] = ['name', 'description', 'type', 'price', 'stock', 'actions'];
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private dialog: MatDialog, private cdRef: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     this.loadProducts();
@@ -29,34 +36,50 @@ export class AddProductComponent implements OnInit {
     localStorage.setItem('products', JSON.stringify(this.products));
   }
 
-  openModal() {
-    this.editingProduct = null;
-    this.showModal = true;
+  openModal(product: Product | null = null) {
+    const dialogRef = this.dialog.open(ProductFormComponent, {
+      width: '400px',
+      data: product,
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        if (product) {
+         this.products = this.products.map(p => (p.id === product.id ? result : p));
+        } else {
+          result.id = new Date().getTime();
+          this.products = [...this.products, result];
+        }
+        this.saveProducts();
+        this.cdRef.detectChanges();
+      }
+    });
   }
 
   editProduct(product: Product) {
-    this.editingProduct = { ...product };
-    this.showModal = true;
+    this.openModal(product);
   }
 
   deleteProduct(id: number) {
     this.products = this.products.filter(product => product.id !== id);
     this.saveProducts();
+    this.cdRef.detectChanges();
   }
 
   handleProductSave(product: Product) {
-    if (this.editingProduct) {
-      this.products = this.products.map(p => (p.id === product.id ? product : p));
-    } else {
+    if (!product.id) {
       product.id = new Date().getTime();
       this.products.push(product);
+    } else {
+      this.products = this.products.map(p => (p.id === product.id ? product : p));
     }
+
     this.saveProducts();
-    this.showModal = false;
   }
 
   toggleView() {
     this.isTableView = !this.isTableView;
+    this.cdRef.detectChanges();
   }
 
   logout() {
--- src/app/product-form/product-form.component.ts
+++ src/app/product-form/product-form.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Inject, Output } from '@angular/core';
+import { Product } from '../models/product.model';
+import { FormsModule } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-form',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, MatDialogModule, MatFormFieldModule, MatInputModule, CommonModule],
   templateUrl: './product-form.component.html',
   styleUrl: './product-form.component.scss'
 })
 export class ProductFormComponent {
+  @Output() onSave = new EventEmitter<Product>();
+  tempProduct: Product = { id: 0, name: '', description: '', type: '', price: 0, stock: 0 };
 
+  constructor(
+    public dialogRef: MatDialogRef<ProductFormComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Product | null
+  ) {
+    if (data) {
+      this.tempProduct = { ...data }
+    }
+  }
+
+  saveProduct() {
+    if (!this.tempProduct.id) {
+      this.tempProduct.id = new Date().getTime();
+    }
+    this.onSave.emit(this.tempProduct);
+    this.dialogRef.close(this.tempProduct);
+  }
+
+  cancel() {
+    this.dialogRef.close();
+  }
 }
